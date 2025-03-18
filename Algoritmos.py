291ecbb28cdef08be7988414594e6cdb4f53fcdd
Pietro285 <picoelho11@gmail.com>
2025-03-17 22:48:54 -0300

    Meu primeiro programa em python.

--- Algoritmos.py
+++ Algoritmos.py
@@ -1,1 +1,20 @@
-print("Hello world!")
+#Exercício 1
+print("Pietro\n")
+
+#Exercício 2
+a = 3
+b = 5
+print(2*a*3*b)
+
+#Exercício 3
+#num1 = 2
+#num2 = 3
+#num3 = 4
+#soma = num1+num2+num3
+#print(soma)
+
+num1 = int(input("Número 1 = "))
+num2 = int(input("Número 2 = "))
+num3 = int(input("Número 3 = "))
+soma = num1+num2+num3
+print(soma)
